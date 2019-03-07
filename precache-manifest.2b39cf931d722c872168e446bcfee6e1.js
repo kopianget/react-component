057296392aee4566a85702fce07de008fb243018
saefullohmaslul <sm@localhost.localdomain>
2019-03-07 20:13:08 +0700

    Updates

---
+++ precache-manifest.2b39cf931d722c872168e446bcfee6e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd6e31a128bc415ad63c",
+    "url": "/react-component/static/js/runtime~main.4464635a.js"
+  },
+  {
+    "revision": "291ae3e58fd5fc93853b",
+    "url": "/react-component/static/js/main.0b5fa44e.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/react-component/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "620ec2b6a739746b2439e85ff6adc64e",
+    "url": "/react-component/index.html"
+  }
+];
